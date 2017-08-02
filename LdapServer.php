c19d53cf1ffe1ae9220628955e008bf846543786
Beshad Talayeminaei <btalayeminaei@developmentgateway.org>
2017-08-02 14:17:14 -0400

    Implemented fetchDN() to find user's DN and bind

--- LdapServer.php
+++ LdapServer.php
@@ -1,19 +1,19 @@
 <?php
 namespace ldap;
 
-class LDAPServerError extends \Exception 
+class LdapServerError extends \Exception
 {
 }
 
-class LDAPAuthError extends \Exception 
+class LdapAuthError extends \Exception
 {
 }
-
 class LdapServer
 {
     private $base;
     private $conn;
     private $filter;
+    private $dn;
 
     public function __construct($user, $pass)
     {
@@ -22,9 +22,43 @@ class LdapServer
         $this->conn = ldap_connect($host, $port);
         $this->filter = sprintf($filter, $user);
 
-        if (!$this->conn) throw new LDAPServerError();
+        if (!$this->conn) throw new LdapServerError();
 
         $result = ldap_set_option($this->conn, LDAP_OPT_PROTOCOL_VERSION, 3);
-        if (!$result) throw new LDAPServerError();
+        if (!$result) throw new LdapServerError();
+
+
+        # search ldap tree for the user's DN
+        $this->dn = $this->fetchDN($this->filter);
+
+        # re-bind with the DN found
+        $result = ldap_bind($this->conn, $this->dn, $pass);
+        if (!$result) throw new LdapAuthError();
+    }
+
+    public function __destruct()
+    {
+        ldap_unbind($this->conn);
+    }
+
+    private function fetchDN($filter)
+    {
+        # bind anonymously first and search for the RDN
+        $result = ldap_bind($this->conn);
+        if (!$result) throw new LdapServerError();
+
+        $result = ldap_search($this->conn, $this->base, $this->filter,
+        array("dn"), 1, 1, 0, LDAP_DEREF_ALWAYS);
+        if (!$result) throw new LdapServerError();
+
+        # get first entry from the search result
+        $first = ldap_first_entry($this->conn, $result);
+        if (!$first) throw new LdapServerError();
+
+        # extract dn from the first entry in the result
+        $dn = ldap_get_dn($this->conn, $first);
+        if (!$dn) throw new LdapServerError();
+
+        return $dn;
     }
 }
