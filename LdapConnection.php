913214c2eef90e363adaf970b738c51ed30bee6c
Styopa Semenukha <ssemenukha@developmentgateway.org>
2017-08-04 11:35:07 -0400

    Wrote base LDAP exception

--- LdapConnection.php
+++ LdapConnection.php
@@ -3,8 +3,14 @@ namespace devgateway\ldap;
 
 use devgateway\ldap\LdapResults;
 
-class LdapConnectionError extends \Exception
+class LdapException extends \RuntimeException
 {
+    public function __construct($connection)
+    {
+        $code = @ldap_errno($connection);
+        $message = @ldap_err2str($code);
+        parent::__construct($message, $code);
+    }
 }
 
 class LdapAuthError extends \Exception
