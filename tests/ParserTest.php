a1a5e5961464f22581481456eb3d9681e518994b
Styopa Semenukha <ssemenukha@developmentgateway.org>
2017-08-17 11:33:39 -0400

    Wrote some parser testcases

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -15,13 +15,55 @@ class ParserTest extends TestCase
     /**
      * @dataProvider descriptionProvider
      */
-    public function testLexer($desc, $tokens)
+    public function testLexer($expected, $desc)
     {
+        $parser = new MockParser($desc);
+        $this->assertEquals($expected, $parser->getTokens());
     }
 
-    public function escapeProvider()
+    public function descriptionProvider()
     {
+        $business_cat = json_decode(
+            '["2.5.4.15","NAME","businessCategory","DESC","RFC2256: business category","EQUALITY"' .
+            ',"caseIgnoreMatch","SUBSTR","caseIgnoreSubstringsMatch", "SYNTAX","1.3.6.1.4.1.1466.' .
+            '115.121.1.15{128}"]'
+        );
+
         return [
+            'inline' => [$business_cat, <<<'EOF'
+( 2.5.4.15 NAME 'businessCategory' DESC 'RFC2256: business category' EQUALITY caseIgnoreMatch SUBSTR caseIgnoreSubstringsMatch SYNTAX 1.3.6.1.4.1.1466.115.121.1.15{128} )
+EOF
+            ],
+            'wrapped' => [$business_cat, <<<'EOF'
+( 2.5.
+ 4.15 NAME 'businessCat
+ egory'
+   DESC '
+ RFC2256: business category
+ '
+  EQUALITY
+   caseIgnoreMatch
+    SUBSTR caseIgno
+ reSubstringsMatch
+  SYNTAX 1.3.6.1.4.1.1466
+ .115.121.1.15{128} )
+EOF
+            ],
+            'wrapped & padded' => [$business_cat, <<<'EOF'
+(   2.5.
+ 4.15    NAME 'businessCat
+ egory'
+   DESC    '
+ RFC2256: business category
+ '   
+  EQUALITY
+   caseIgnoreMatch
+    SUBSTR                            caseIgno
+ reSubstringsMatch
+  SYNTAX     1.3.6.1.4.1.1466
+ .115.121.1.15{128}    )
+EOF
+            ]
         ];
     }
 }
