cd7e78ee8f7d7efa71b98ebc177068758bef3090
Styopa Semenukha <ssemenukha@developmentgateway.org>
2017-08-25 12:41:45 -0400

    Objects use multiple inheritance

--- ObjectDefinition.php
+++ ObjectDefinition.php
@@ -23,14 +23,6 @@ class ObjectDefinition extends Definition
 
     public function __construct(Schema $schema, array $definition)
     {
-        // find my superclass
-        if (array_key_exists('sup', $definition) {
-            $sup_name = strtolower($definition['sup']);
-            $sup = $sup_name == 'top' ? null : $schema[$sup_name];
-        } else {
-            $sup = null;
-        }
-
         foreach (['may', 'must'] as $may_or_must) {
 
             // ensure my own lists exist
@@ -42,14 +34,22 @@ class ObjectDefinition extends Definition
             $attribute_names = &$definition[$may_or_must]; // alias for readability
             $attributes = [];
             foreach ($attribute_names as $name) {
-                $attributes[] = $schema[$name];
+                $attribute = $schema[$name];
+                $id = spl_object_hash($attribute);
+                // imitate a hash, so that array_merge returns unique elements
+                $attributes[$id] = $attribute;
             }
 
-            // inherit lists from my superclass
-            if (!is_null($sup)) {
-                $all_attributes = array_merge($attributes, $sup->properties[$may_or_must]);
-                $definition[$may_or_must] = array_unique($all_attributes, SORT_REGULAR);
+            // push my attributes into an array
+            $args = [$attributes];
+            foreach ($definition['sup'] as $sup_name) {
+                $sup = $schema[$sup_name];
+                // push my parent's attributes into an array
+                $args[] = $sup->properties[$may_or_must];
             }
+
+            // merge my own and my parents' attributes
+            $definition[$may_or_must] = call_user_func_array('array_merge', $args);
         }
 
         // remove inherited MAYs which have been redeclared as MUSTs
