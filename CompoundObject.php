b16e8209aedc3a65f01988d357858edfa576f4c4
Beshad Talayeminaei <btalayeminaei@developmentgateway.org>
2017-09-05 17:14:55 -0400

    Implemented CompountObject constructor.

--- CompoundObject.php
+++ CompoundObject.php
@@ -10,8 +10,23 @@
 namespace devgateway\ldap;
 
 use devgateway\ldap\SimpleObject;
+use devgateway\ldap\Schema;
+use devgateway\ldap\SimpleObject;
 
-class CompoundObject
+class CompoundObject extends OidArray
 {
+    public function __construct(Schema $schema, array $entry)
+    {
+        for ($i=0; $i < $entry["count"]; $i++) {
+            $key = $entry[$i];
+            if (strtolower($key) == "objectclass") {
+                for ($j = 0; $j<$entry[$key]['count']; $j++) {
+                    $simple_object = new SimpleObject($schema, $entry[$key][$j], $entry);
+                    $offset = OidArray::offsetMake($simple_object);
+                    $this[$offset] = $simple_object;
+                }
+            }
+        }
+    }
 }
 
