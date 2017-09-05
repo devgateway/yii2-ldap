29cdb03bc21bc91f5074459258dfd8ddb96da30b
Styopa Semenukha <ssemenukha@developmentgateway.org>
2017-09-04 23:39:58 -0400

    Added files for object classes

---
+++ CompoundObject.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * CompoundObject class
+ *
+ * @link https://github.com/devgateway/yii-com-ldap
+ * @copyright 2017, Development Gateway, Inc
+ * @license GPL, version 3
+ */
+
+namespace devgateway\ldap;
+
+use devgateway\ldap\SimpleObject;
+
+class CompoundObject
+{
+}
+
---
+++ SimpleObject.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * SimpleObject class
+ *
+ * @link https://github.com/devgateway/yii-com-ldap
+ * @copyright 2017, Development Gateway, Inc
+ * @license GPL, version 3
+ */
+
+namespace devgateway\ldap;
+
+use devgateway\ldap\AttributeDefinition;
+use devgateway\ldap\OidArray;
+
+class SimpleObject
+{
+}
+
