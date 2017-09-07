ef2c609312b60f10121ab4ba04693f3c5a9c29ea
Beshad Talayeminaei <btalayeminaei@developmentgateway.org>
2017-09-07 15:36:48 -0400

    Simple tests for _each(), _either(), _any() functions

--- tests/FilterBuilderTest.php
+++ tests/FilterBuilderTest.php
@@ -9,6 +9,7 @@ class FilterBuilderTest extends TestCase
         $test_or = FilterBuilder::_or(['objectClass' => ['device', 'user']]);
         $this->assertEquals($test_or, "(|(objectClass=device)(objectClass=user))");
     }
+
     public function testAnd()
     {
         $test_and_one = FilterBuilder::_and(['objectClass' => ['device', 'user']]);
@@ -17,11 +18,13 @@ class FilterBuilderTest extends TestCase
         $this->assertEquals($test_and_one, "(&(objectClass=device)(objectClass=user))");
         $this->assertEquals($test_and_two, "(&(objectClass=x)(objectClass=y))");
     }
+
     public function testNot()
     {
         $test_not = FilterBuilder::_not(['drink' => 'liquor']);
         $this->assertEquals($test_not, "(!(drink=liquor))");
     }
+
     public function testCombined()
     {
         $test_and_one = FilterBuilder::_and(['objectClass' => ['device', 'user']]);
@@ -56,6 +59,24 @@ class FilterBuilderTest extends TestCase
         $this->assertEquals($COMBINED_ONE, "(&(|(drink=whiskey)(drink=gin)(drink=rum))(age<=21))");
         $this->assertEquals($COMBINED_TWO, "(&(objectClass=x)(objectClass=y)(age<=21))");
 
+    }
+
+    public function testEach()
+    {
+        $each = FilterBuilder::_each(['givenName', 'surname', 'uid'], 'adam');
+        $this->assertEquals($each, "(&(givenName=adam)(surname=adam)(uid=adam))");
+    }
+
+    public function testEither()
+    {
+        $either = FilterBuilder::_either(['givenName', 'surname', 'uid'], 'adam');
+        $this->assertEquals($either, "(|(givenName=adam)(surname=adam)(uid=adam))");
+    }
+
+    public function testAny()
+    {
+        $any = FilterBuilder::_any(['givenName', 'surname', 'uid'], " adam  smi");
+        $this->assertEquals($any, "(|(givenName=*adam*)(givenName=*smi*)(surname=*adam*)(surname=*smi*)(uid=*adam*)(uid=*smi*))");
     }
 
 }
