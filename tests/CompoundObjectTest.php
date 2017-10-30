7b390fe235def8ef136c2103f36d50f787d56434
Styopa Semenukha <ssemenukha@developmentgateway.org>
2017-10-30 15:25:57 -0400

    Wrote a unit test for array sorting

---
+++ tests/CompoundObjectTest.php
@@ -0,0 +1,62 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use devgateway\ldap\CompoundObject;
+use devgateway\ldap\SimpleObject;
+
+class MockableSimpleObject extends SimpleObject
+{
+    public function __construct($mock)
+    {
+        foreach ($mock as $name => $value) {
+            $offset = ['1.2.3', $name];
+            $this[$offset] = $value;
+        }
+    }
+}
+
+class MockableCompoundObject extends CompoundObject
+{
+    public function __construct($mock)
+    {
+        foreach ($mock as $class_name => $attrs) {
+            $offset = ['1.2.3', $class_name];
+            $this[$offset] = new MockableSimpleObject($attrs);
+        }
+    }
+}
+
+class CompoundObjectTest extends TestCase
+{
+    /**
+     * @dataProvider mockProvider
+     */
+    public function testSort($unsorted, $expected)
+    {
+        var_dump($expected);
+        $comp_object = new MockableCompoundObject($unsorted);
+        $sorted = $comp_object->toArray();
+        var_dump($sorted);
+        $this->assertTrue($sorted === $expected);
+    }
+
+    public function mockProvider()
+    {
+        return [ // array of test cases
+            [ // array of arguments
+                ['inetOrgPerson' => [
+                    'surname' => 'Doe',
+                    'givenName' => 'John',
+                    'userId' => 42,
+                    'mail' => 'john@example.net'
+                ]],
+                ['inetOrgPerson' => [
+                    'givenName' => 'John',
+                    'mail' => 'john@example.net',
+                    'surname' => 'Doe',
+                    'userId' => 42
+                ]]
+            ],
+        ];
+    }
+}
+
